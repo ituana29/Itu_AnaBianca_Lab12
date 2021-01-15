348ed42b2ff5d2d5000126cd8d7dfbcbc04c537c
Bianca <itu.bianca11@yahoo.com>
2021-01-15 09:37:51 -0800

    Add project files.

---
+++ Itu_AnaBianca_Lab12/Itu_AnaBianca_Lab12/ListPage.cs
@@ -0,0 +1,10 @@
+﻿using Itu_AnaBianca_Lab12.Models;
+using Xamarin.Forms;
+
+namespace Itu_AnaBianca_Lab12
+{
+    internal class ListPage : Page
+    {
+        public ShopList BindingContext { get; set; }
+    }
+}
